ea42e6c61ca2ed6584edf6b461d90badb1be929c
backspace <backspace@backblog.me>
2018-10-16 15:51:46 +0800

    add cronie deps in test

--- test.sh
+++ test.sh
@@ -7,4 +7,4 @@ if  [ -z $PKG_NAME ]; then
 fi
 # docker run -it --rm  -v $(pwd)/RPMS:/rpms -p 9999:9999 -p 80:80 centos:7
 docker run -it --rm  -v $(pwd)/RPMS:/rpms centos:7 /bin/bash -c \
-    "yum install perl -y && cd /rpms/x86_64 && rpm -i $PKG_NAME && cd /home/admin/honeycomb && su admin -c \"./bin/server_ctl start\" && curl http://localhost:9999 && cat /home/admin/honeycomb/target/honeycomb/package.json"
+    "yum install perl -y && yum install cronie -y && cd /rpms/x86_64 && rpm -i $PKG_NAME && cd /home/admin/honeycomb && su admin -c \"./bin/server_ctl start\" && curl http://localhost:9999 && cat /home/admin/honeycomb/target/honeycomb/package.json"
